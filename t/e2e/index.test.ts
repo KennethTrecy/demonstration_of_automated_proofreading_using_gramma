e17de85e4e0bc42d86d9f714729a0605903ef8c2
Kenneth Trecy Tobias <web.kennethtobias@gmail.com>
2023-06-03 13:23:03 +0800

    test(e2e): check the grammar

--- t/e2e/index.test.ts
+++ t/e2e/index.test.ts
@@ -1,9 +1,23 @@
-import { test } from "@playwright/test"
+import { check } from "gramma"
+import { test, expect } from "@playwright/test"
 
 test("page has correct grammar", async ({ page }) => {
 	await page.goto("/")
 
 	const allTexts = await page.locator(`css=p`).allInnerTexts()
 
-	console.log(allTexts)
+	const pendingResults: Promise<any>[] = allTexts.map(async text => {
+		const result = await check(text, {
+			"api_url": "http://0.0.0.0:8081/v2/check",
+
+			// Put the words in this array that are not in the English dictionary like names.
+			"dictionary": []
+		})
+
+		return result.matches
+	})
+
+	const expectedMatches = allTexts.map(() => [])
+	const matches = await Promise.all(pendingResults)
+	await expect(matches).toEqual(expectedMatches)
 });
